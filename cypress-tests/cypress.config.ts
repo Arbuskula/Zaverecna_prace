866d69d70745c4d36abfc569c767420752927d36
Veronika <veronikakadlecova@volny.cz>
2025-06-05 19:40:35 +0200

    New

--- cypress-tests/cypress.config.ts
+++ cypress-tests/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   watchForFileChanges: false,
+  projectId: "k6apm6",
   e2e: {
     baseUrl: "https://www.legenio.cz/",
     setupNodeEvents(on, config) {
